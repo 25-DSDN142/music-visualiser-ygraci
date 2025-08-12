d72fe435c3c6bbdfda0248f78f48dc32bae4a8e4
ygraci <ygracianne@gmail.com>
2025-08-11 14:05:16 -1000

    PBA 4 - Loops using Birds

--- music_starter.js
+++ music_starter.js
@@ -1,15 +1,27 @@
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  colorMode (HSB, 100);
-  background(255);
-  rectMode(CENTER);
-  strokeWeight(9);
-  stroke(drum,80,80);
 
-  var drumMap =map(drum, 0, 100, 30, 90);
-  var lengthOfLine = 300; 
-  var LineStart = 100;
-  var lineEnd = LineStart+lengthOfLine;
+  // Loop to draw 5 birds
+  for (let i = 0; i < 5; i++) {
+    let xPos = drum + 100 + i * 80; 
+    let yPos = 100 + i * 10;
+    bird(xPos, yPos);
 
-  line(LineStart, 300, lineEnd, 300);
+function bird (birdX, birdY) {
+
+  noFill();
+  strokeWeight(2);
+  stroke(0);
+//code to make the birds 
+//left wing
+  arc (birdX+23, birdY+9.5, 30, 19, 290, 350, CHORD);
+
+//middle part 
+  ellipse (birdX+36.4, birdY+5.5, 2.1, 2.2);
+
+//right wing
+  arc (birdX+50, birdY+7, 29, 17, 180, 230, CHORD);
 }
+  }
+}
+
