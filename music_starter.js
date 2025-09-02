139e89ae26b81ecd72194c9b510dd3a67e1d9a26
ygraci <ygracianne@gmail.com>
2025-09-01 14:03:29 -1000

    test

--- music_starter.js
+++ music_starter.js
@@ -1,27 +1,27 @@
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
+  background(255);
+  
 
   // Loop to draw 5 birds
-  for (let i = 0; i < 5; i++) {
-    let xPos = drum + 100 + i * 80; 
-    let yPos = 100 + i * 10;
-    bird(xPos, yPos);
+for (let i = 0; i < 5; i++) {
+    let areaX = drum + 100 + i * 80; 
+    let areaY = 100 + i-10;
+    bird(areaX, areaY);
 
 function bird (birdX, birdY) {
-
   noFill();
   strokeWeight(2);
   stroke(0);
-//code to make the birds 
 //left wing
   arc (birdX+23, birdY+9.5, 30, 19, 290, 350, CHORD);
-
 //middle part 
   ellipse (birdX+36.4, birdY+5.5, 2.1, 2.2);
-
 //right wing
   arc (birdX+50, birdY+7, 29, 17, 180, 230, CHORD);
-}
+    }
   }
+
+  
 }
 
--- music_starter_Practice.js
+++ music_starter_Practice.js
@@ -40,3 +40,29 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
    textSize(vocal);
    text(words, width/2, height/3);
 }
+
+
+//Random things to practice 
+  // Loop to draw 5 birds
+for (let i = 0; i < 5; i++) {
+    let areaX = drum + 100 + i * 80; 
+    let areaY = 100 + i-10;
+    bird(areaX, areaY);
+    
+
+function bird (birdX, birdY) {
+
+  noFill();
+  strokeWeight(2);
+  stroke(0);
+
+//left wing
+  arc (birdX+23, birdY+9.5, 30, 19, 290, 350, CHORD);
+
+//middle part 
+  ellipse (birdX+36.4, birdY+5.5, 2.1, 2.2);
+
+//right wing
+  arc (birdX+50, birdY+7, 29, 17, 180, 230, CHORD);
+}
+  }
